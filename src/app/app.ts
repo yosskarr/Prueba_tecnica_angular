9fd6b9ef394883d8b4d1ee95cd54291c37e1fd61
yosskarr <yoskaridler@gmail.com>
2025-09-29 16:59:49 -0500

    feat(lesson-2): Create a home component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
 import {Component} from '@angular/core';
+import {Home} from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Home],
   template: `
-    <h1>Hello world</h1>
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.css'],
 })
